7040bb928f6ec50e3e5ed3badb4847141aaddab4
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-01 08:42:48 -0500

    finished

---
+++ Admission/Admission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Admission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your GPA: ");
+            decimal.TryParse(Console.ReadLine(), out decimal enteredGPA);
+            Console.Write("Enter your admission test score: ");
+            int.TryParse(Console.ReadLine(), out int enteredScore);
+
+            if ((enteredGPA < 3.0M && enteredScore >= 80) || (enteredGPA >= 3.0M && enteredScore >= 60))
+            {
+                Console.WriteLine("Accept");
+            } else {
+                Console.WriteLine("Reject");
+            }
+        }
+    }
+}
